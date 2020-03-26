7514ed5b8c023aca89d6172eecbbd6c0fcaa0038
Paulo Henrique <phenriq694@gmail.com>
2020-03-26 20:40:00 -0300

    BACKEND: Corrigindo rota para fazer logon.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -29,7 +29,7 @@ const SessionController = require('./controllers/SessionController');
    * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos;
    */
 
-routes.post('/', SessionController.create)
+routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
