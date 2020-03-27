e7855ed561be1bcc6568fe368b3b526f0a3dc973
Paulo Henrique <phenriq694@gmail.com>
2020-03-27 17:21:20 -0300

    MOBILE - Adicionando a funcionalidade de rolagem infinita para passar as páginas da listagem de casos.

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -12,6 +12,8 @@ import styles from './styles';
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const navigation = useNavigation();
 
@@ -20,10 +22,24 @@ export default function Incidents() {
   }
 
   async function loadIncidents() {
-    const res = await api.get('incidents');
+    if (loading) {
+      return;
+    }
 
-    setIncidents(res.data);
+    if (total > 0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true);
+
+    const res = await api.get('incidents', {
+      params: { page }
+    });
+
+    setIncidents([...incidents, ...res.data]);
     setTotal(res.headers['x-total-count']);
+    setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -47,6 +63,8 @@ export default function Incidents() {
         style={styles.incidentList}
         keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
